048b1fb5c32955ca049b04fa4f5d18584968d1b2
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2022-02-27 15:53:33 -0300

    authentication controller creation

---
+++ IdentityAuthAPI/Controllers/AuthController.cs
@@ -0,0 +1,109 @@
+﻿using IdentityAuthAPI.Extensions;
+using IdentityAuthAPI.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityAuthAPI.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly AppSettings _appSettings;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, 
+                                UserManager<IdentityUser> userManager, 
+                                IOptions<AppSettings> appSettings)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _appSettings = appSettings.Value;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(RegisterUserViewModel registerUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+
+            if (result.Succeeded)
+            {
+                //isPersistent = false significa que não quer armazenar as informações para facilitar o próximo login 
+                await _signInManager.SignInAsync(user, false);
+                return Ok(GenerateJwt());
+            }
+
+            return Ok(result.Errors);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(LoginUserViewModel loginUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(loginUser.Email, loginUser.Password, false, true);
+
+            if (result.Succeeded)
+            {
+                return Ok(GenerateJwt());
+            }
+
+            if (result.IsLockedOut)
+            {
+                return BadRequest();
+            }
+
+            return BadRequest();
+        }
+
+        private string GenerateJwt()
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+
+            var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = _appSettings.Issuer,
+                Audience = _appSettings.ValidIn,
+                Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiresIn),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+            });
+
+            var encodedToken = tokenHandler.WriteToken(token);
+
+            return encodedToken;
+        }
+
+        [HttpGet("teste")]
+        public ActionResult Teste(LoginUserViewModel loginUser)
+        {
+            return Ok("Teste");
+        }
+    }
+}
