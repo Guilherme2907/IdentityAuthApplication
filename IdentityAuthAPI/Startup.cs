efbb956df5ede77e8d1d7fd6d5486f29fb6f6277
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2022-02-27 15:55:07 -0300

    Settings for identity framework and startup

--- IdentityAuthAPI/Startup.cs
+++ IdentityAuthAPI/Startup.cs
@@ -1,3 +1,4 @@
+using IdentityAuthAPI.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,8 @@ namespace IdentityAuthAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddIdentityConfiguration(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +42,8 @@ namespace IdentityAuthAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
